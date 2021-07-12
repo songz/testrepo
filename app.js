c9b3243d75e30beead7d7a205154b1c406a54cde
Chi Kim <chi@kim.club>
2021-07-12 11:58:43 -0700

    website setup

--- app.js
+++ app.js
@@ -12,19 +12,4 @@ app.post("/help", (req, res) => {
 
 app.use(express.static("/home/chikim/Documents/leet/"));
 
-app.get("/", (req, res) => {
-  res.send(`
-  <h1>Leet code solutions</h1>
-
-  <p>Type functionName.test.js</p>
-
-  <p>For Example,</p>
-  <p>
-  <a href="/minInterval.test.js" target="_blank">minInterval.test.js</a>
-  for 
-<a href="https://leetcode.com/contest/weekly-contest-239/problems/minimum-interval-to-include-each-query/">1851: Minimum Interval to Include Each Query</a> Leetcode problem
-  </p>
-  `);
-});
-
-app.listen(3020);
+app.listen(3020); // l33t.songz.dev or l33t.ameyz.dev
